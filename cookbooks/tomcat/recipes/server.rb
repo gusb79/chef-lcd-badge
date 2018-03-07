d996c327687cae811fa65163b6e33e46df9436d4
Gustavo Benitez <gustavo.benitez@ibm.com>
2018-03-07 16:05:11 +0000

    fixes

--- cookbooks/tomcat/recipes/server.rb
+++ cookbooks/tomcat/recipes/server.rb
@@ -3,8 +3,11 @@
 # Recipe:: server
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-default['server']['user'] = 'tomcat'
-default['server']['group'] = 'tomcat'
+
+# ERROR: Node attributes are read-only when you do not specify which precedence
+# level to set. To set an attribute use code like `node.default["key"] = "value"'
+node.default['server']['user'] = 'tomcat'
+node.default['server']['group'] = 'tomcat'
 
 package 'java-1.7.0-openjdk-devel'
 
@@ -19,18 +22,20 @@ directory '/opt/tomcat' do
   group node['server']['group']
 end
 
-remote_file 'Tomcat Binaries' do
-  source 'http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5.28.tar.gz'
-  path '/opt/tomcat'
-  not_if { ::File.exist?('/opt/tomcat/conf') }
-  notifies :run, execute['untar']
-end
-
 execute 'untar' do
   cwd '/opt/tomcat'
   command 'sudo tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
   action :nothing
 end
+
+remote_file 'Tomcat Binaries' do
+  source 'http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5.28.tar.gz'
+  path '/opt/tomcat/apache-tomcat-8.5.28.tar.gz'
+  not_if { ::File.exist?('/opt/tomcat/conf') }
+  notifies :run, 'execute[untar]', :immediately
+end
+
+
 
 
 directory '/opt/tomcat/conf' do
@@ -61,9 +66,11 @@ template 'tomcat.service' do
   source 'tomcat.service.erb'
   path '/etc/systemd/system/tomcat.service'
   action [:create_if_missing]
+  # Does not work with Docker
   notifies :run, 'execute[daemon-reload]', :immediately
 end
 
+# Does not work in Docker
 execute 'daemon-reload' do
   command 'sudo systemctl daemon-reload'
   action :nothing
